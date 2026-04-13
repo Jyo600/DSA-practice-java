2ad9058831c1da781493b45cb651583ec4289fcb
jyoti gavade <jyotigavade138@gmail.com>
2026-04-13 22:43:32 +0530

    Add files via upload
    updated variables in sliding window for better understanding

--- variableslidingwindow.java
+++ variableslidingwindow.java
@@ -2,22 +2,23 @@ import java.io.*;
 import java.util.*;
 class variableslidingwindow{
     public static void main(String args[]){
-        int[]nums={1,2,1,0,1,1,0};
-        int k=4;
+        int[]nums={4,1,1,1,2,3,5};
         int sum=0;
         int left=0;
         int maxlen=0;
-        for(int right=0;right<nums.length;right++){
-            sum+=nums[right];
+        int k=5;
         
-        while(sum>k)
-        {
+        for(int right=0;right<nums.length;right++){
+             sum+=nums[right];
+        
+        while(sum>k){
             sum-=nums[left];
             left++;
         }
-        maxlen=Math.max(maxlen,right-left+1);
+            maxlen=Math.max(maxlen,right-left+1);
+        
     }
-    System.out.println(maxlen);
+        System.out.println(maxlen);
 
     }
 }
