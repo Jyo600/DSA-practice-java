1f158aa3b952388d93f75725dc37d8c462f8494b
jyoti gavade <jyotigavade138@gmail.com>
2026-06-14 10:39:21 +0530

    Add files via upload
    Solve Two Sum Problem Using HashMap and Complement Approach

--- twosum.java
+++ twosum.java
@@ -1,23 +1,18 @@
 import java.io.*;
 import java.util.*;
 class twosum{
-   public static int[]twosum(int[]nums,int target){
-    for(int i=0;i<nums.length;i++){
-        for(int j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return new int[]{i,j};
-            }
-        }
+    public static void main(String[]args){
+       int[] nums = {2,7,11,15};
+       int target = 9;
+       HashMap<Integer,Integer>map=new HashMap<>();
+       for(int i=0;i<nums.length;i++){
+        int complement=target-nums[i];
+       
+       if(map.containsKey(complement)){
+         System.out.println(map.get(complement)+" "+i);
+    
+       }
+    
+    map.put(nums[i],i);
     }
-    return new  int[]{};
-
-
-    }
-    public static void main(String args[]){
-        int[]nums={11,17,2,7};
-        int target=9;
-        int[] result= twosum(nums,target);
-        System.out.println("the indices are" +result[0]+ "second indices is" +result[1]);
-
-    }
-}
+}}
