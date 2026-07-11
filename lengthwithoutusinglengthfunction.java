8115ec9ebdaf9713095e29addb2e36bab838cc20
jyoti gavade <jyotigavade138@gmail.com>
2026-07-11 22:40:16 +0530

    Add files via upload
    it is the basic concept but most powerful

---
+++ lengthwithoutusinglengthfunction.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class lengthwithoutusinglengthfunction{
+      public static void main(String[]args){
+         String name="jyoti";
+         int count=0;
+        
+         char[]ch=name.toCharArray();
+          while(count==0) {         
+           count++; 
+           } 
+         System.out.println(count);   
+       }  
+     }
