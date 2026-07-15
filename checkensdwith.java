6c1506db6d0c816654cb79dbeef8e01211791c9e
jyoti gavade <jyotigavade138@gmail.com>
2026-07-15 07:45:38 +0530

    Add files via upload

---
+++ checkensdwith.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class checkensdwith{
+      public static void main(String[]args){
+            String name="jyotigavade@gmail.com";
+            boolean result=name.endsWith(".com");
+                System.out.println(result);
+
+         }
+      }
