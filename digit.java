0eea3657a06a379808a504acbb8da96bfbac251e
jyoti gavade <jyotigavade138@gmail.com>
2026-07-21 22:03:31 +0530

    Add files via upload

---
+++ digit.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class digit{
+       public static void main(String[]args){
+        String s="1321";
+         String ch="";
+           for(int i=0;i<s.length()-1;i++){
+
+            ch+=s.charAt(i);
+       }
+ System.out.println(ch);
+ 
+       }
+ 
+   }
